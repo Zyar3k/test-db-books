f0163392ae03676a923d13914d5215904ea11d04
zyar3k <a.zygan@gmail.com>
2023-01-20 11:10:35 +0100

    Add project

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+require("express-async-errors");
+const dotenv = require("dotenv");
+dotenv.config();
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const app = express();
+const connectDB = require("./db/connect");
+const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6tpkm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 8888;
+const authRouter = require("./routes/auth");
+const booksRouter = require("./routes/books");
+
+const notFoundMiddleware = require("./middleware/not-found.js");
+const errorHandlerMiddleware = require("./middleware/error-handler.js");
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+app.use("/admin/auth", authRouter);
+app.use("/admin/books", booksRouter);
+
+app.use("/", booksRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const start = async () => {
+  try {
+    await connectDB(mongoUri);
+    app.listen(PORT, () =>
+      console.log(`Server started on port http://localhost:${PORT}`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
